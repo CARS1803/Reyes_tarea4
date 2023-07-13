2b4be677739dbd9e2fed34b887e27db63fd1e59a
Reyes <Carlosreyes1803@gmail.com>
2023-07-13 11:13:54 -0600

    configuraciones a el codigo html, para crear divs y un boton para eliminar los inputs extras, asi mismo se agregaron las funciones a js.

--- Codigo.js
+++ Codigo.js
@@ -1,14 +1,42 @@
 function Input() {
-    let input = document.createElement('input')
-    input.type = 'text'
-    input.name = 'input[]'
-    input.placeholder = 'Ingrese sus datos'
+    let input = document.createElement('input');
+    input.type = 'text';
+    input.name = 'input[]';
+    input.placeholder = 'Ingrese sus datos';
 
-    let eliminar = document.createElement('button')
-    eliminar.type = 'button'
-    eliminar.innerHTML = 'eliminar'
+    let eliminar = document.createElement('button');
+    eliminar.type = 'button';
+    eliminar.innerHTML = 'eliminar';
     eliminar.onclick = function() {
-        eliminarInput(input)
-    }
+        eliminarInput(input);
+    };
+
+    let container = document.getElementById("inputsContainer");
+    container.appendChild(input);
+    container.appendChild(eliminar);
+    container.appendChild(document.createElement("br"));
 }
 
+function eliminarInput(input) {
+    let container = document.getElementById('inputsContainer');
+    container.removeChild(input.previousSibling);
+    container.removeChild(input.nextSibling);
+    container.removeChild(input);
+}
+
+document.getElementById('Formulario').addEventListener('submit', function(event) {
+    let inputs = document.getElementsByName('input[]');
+    let validacion = true;
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].value === '') {
+            inputs[i].classList.add('error');
+            validacion = false;
+        }
+    }
+    
+    if (!validacion) {
+        event.preventDefault();
+        alert('Por favor, llenar todos los inputs.');
+    }
+});
